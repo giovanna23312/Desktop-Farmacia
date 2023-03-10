cc186a27602d50be5dda7fc8ea8ed29c4b12dd18
giovanna23312 <giovannapintor4@gmail.com>
2023-03-10 11:51:34 -0300

    Tela de Splash e Login

---
+++ FarmaVital/FarmaVital/FrnLogin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FarmaVital
+{
+    public partial class FrnLogin : Form
+    {
+        public FrnLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((textBox1.Text == "giovanna") && (textBox2.Text == "8825"))
+            {
+                this.Visible = false;
+                Form2 newform3 = new Form2();
+                newform3.ShowDialog();
+            }
+            else
+                MessageBox.Show("Usuário ou Senha Inválido", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
